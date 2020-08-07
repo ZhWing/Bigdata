1809e778709845e7cb3e12ba0ac7980cc69c443e
ZhW <zhw980924@outlook.com>
2020-08-07 15:53:24 +0800

    Spark 常用算子

--- src/main/java/cn/zhw/spark/day1/SparkReduce.scala
+++ src/main/java/cn/zhw/spark/day1/SparkReduce.scala
@@ -1,5 +1,6 @@
 package cn.zhw.spark.day1
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkReduce {
@@ -9,10 +10,10 @@ object SparkReduce {
       .setAppName("SparkSample")
     val sc = new SparkContext(conf)
 
-    val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val RDD = sc.parallelize(list)
-    val sum = RDD.reduce(_ + _)
-    val sum1 = RDD.sum()
-    println(sum + " "  + sum1)
+    val list: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val RDD: RDD[Int] = sc.parallelize(list)
+    val sum: Int = RDD.reduce(_ + _)
+    val sum1: Double = RDD.sum()
+    println(sum + " " + sum1)
   }
 }
